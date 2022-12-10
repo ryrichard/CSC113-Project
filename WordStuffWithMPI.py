2e51137c52ea3b436813b1b2e617c19147f82e1d
ryrichard <66878091+ryrichard@users.noreply.github.com>
2022-12-10 12:44:42 -0500

    Added Text and .py

--- WordStuffWithMPI.py
+++ WordStuffWithMPI.py
@@ -21,6 +21,11 @@
 import os
 import threading
 import time
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+worker = comm.Get_rank()
 
 def stripWord(word):
     for i in range(len(word) - 1, -1, -1):
@@ -47,19 +52,21 @@ def countWords(title, result, seconds, dict):
 
     # print("Total found in ", title, ": ", wordCount, end="\n")
     result.append(wordCount)
-    seconds.append(time.time() - start)
+    finalTime = time.time() - start
+    seconds.append(finalTime)
+    print(f"{title} took {finalTime} ms")
 
 
 # f = open("LesMiserablesbyVictorHugo", 'r') #open a readable file
 
-Num_Of_Threads = 8
+Num_Of_Threads = worker + 1 #cant have zero threads
 title = "LesMiserablesbyVictorHugo.txt"
 title_list = []
 text = title.split('.')
 
 print("Creating titles")
 for i in range(Num_Of_Threads):
-    newTitle = text[0] + str(i) + "." + text[1]
+    newTitle = text[0] + "_" + str(worker + 1) + "_" +  str(i) +  "." + text[1] #change title to include associated worker (title_worker#_thread)
     print(newTitle)
     title_list.append(newTitle)
 
@@ -70,7 +77,7 @@ lines = f.readlines()
 w = [] 
 SIZE = int(len(lines))
 split = (int(SIZE / Num_Of_Threads)) + 1 #better to go over then under as going under can potentially cut out lines of text
-print(f"Total: {SIZE}, split: {split}")
+# print(f"Total: {SIZE}, split: {split}")
 
 print("Splitting the text")
 for i in range(Num_Of_Threads):
@@ -79,7 +86,7 @@ for i in range(Num_Of_Threads):
     start = i * split
     end = start + split
     if end > SIZE: end = SIZE
-    print(f"Start: {start}, End: {end}")
+    # print(f"Start: {start}, End: {end}")
     for line in lines[start:end]:
         # print(line, end="")
         w.write(line)
@@ -90,28 +97,36 @@ threads = []
 result = []
 seconds = []
 dict = {}
+startTime = time.time() #start time to do all threads
 
 print("Starting to count")
 for i in range(Num_Of_Threads):
-    print("Counting in ", title_list[i])
+    # print("Counting in ", title_list[i])
     threads.append(threading.Thread(target=countWords, args=(title_list[i], result, seconds, dict, )))
     threads[i].start()
 
 for i in range(Num_Of_Threads):
     threads[i].join()
-    print(f"End thread[{i}] in {seconds[i]} seconds")
+    # print(f"End thread[{i}] in {seconds[i]} seconds")
     wordCount += result[i]
 
-print("Total words: ", wordCount)
+finalTime = time.time() - startTime
+data = comm.gather(finalTime, root = 0)
+
+# print("Total words: ", wordCount)
+
+#deleting flies
+for i in title_list:
+    os.remove(i)
 
 # sortedList = sorted([(values, keys) for (keys, values) in dict.items()], reverse=True)
 
 # for i in sortedList:
 #     print(i[0], i[1])
 
-for i in dict:
-    print(i, dict[i])
+# for i in dict:
+#     print(i, dict[i])
 
-#deleting files
-for i in title_list:
-    os.remove(i)
+if worker == 0:
+    for i in range(len(data)):
+        print(f"Worker {i} took {data[i]} ms")
